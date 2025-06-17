ad468f1dbef18437ad5ee6d957d645d6454b6b8b
Suchith Sandunika <esuchith@gmail.com>
2025-06-17 23:34:47 +0530

    fixed cv google drive link

--- src/data/constants.js
+++ src/data/constants.js
@@ -27,7 +27,7 @@ export const Bio = {
   description:
     "A person with a strong foundation in Computer Science Fundamentals, a strong mindset and a good devotion to each work I do. I am also with a good      knowledge about English, Programming, Frameworks, technologies and many things related to Software Engineering field.",
   github: "https://github.com/suchith-sandunika",
-  resume: "https://drive.google.com/file/d/11OYRXcxDqOmfHfWfs_W9IkG1tq1ErR0c/view?usp=drive_link",
+  resume: "https://drive.google.com/file/d/1MG1QP-e6pbK9abTFSZpLATuAqwhbfQ5c/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/suchith-sandunika",
   twitter: "https://twitter.com/esuchith",
   instagram: "https://www.instagram.com/_suchith_sandunika_",
